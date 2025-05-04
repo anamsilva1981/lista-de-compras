d7db069ccf9a0bea62f06e4fc4aa11345fef968d
anamsilva1981 <ana.msilva@hotmail.com>
2025-05-04 08:42:27 -0300

    adiciona funcionalidade para adicionar itens à lista de compras

--- src/app/lista-compras/lista-compras.component.ts
+++ src/app/lista-compras/lista-compras.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Item } from './item';
 
 @Component({
   selector: 'lista-compras',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
+
+  public item: string = '';
+  public lista: Item[] = [];
+
+
+  public adicionarItem(){
+    let itemLista = new Item();
+
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
+    this.item = '';
+
+    console.table(this.lista);
+  }
 
 }
